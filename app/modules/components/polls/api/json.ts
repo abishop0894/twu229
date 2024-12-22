9dddcfe174e0a656360cf4a168759da7fbe38e28
abishop0894 <abishop0894@gmail.com>
2024-12-22 14:57:04 -0500

    new

---
+++ app/modules/components/polls/api/json.ts
@@ -0,0 +1,32 @@
+export const json = {
+  "title": "TWU 229 Monthly Poll",
+  "description": "ends 12/25/24",
+  "logo": "https://api.surveyjs.io/private/Surveys/files?name=7e6959bc-3c34-410a-90fc-f1cebd7933f7",
+  "logoHeight": "300px",
+  "logoPosition": "right",
+  "pages": [
+    {
+      "name": "page1",
+      "elements": [
+        {
+          "type": "rating",
+          "name": "question1",
+          "title": "On a scale of 1-10, rank the unions performance as of late.",
+          "isRequired": true
+        },
+        {
+          "type": "text",
+          "name": "question2",
+          "title": "What is something you'd like to see done by the union?",
+          "isRequired": true
+        },
+        {
+          "type": "text",
+          "name": "question3",
+          "title": "What is the likelihood of you suggestion to non-unionized coworkers to join the union?",
+          "isRequired": true
+        }
+      ]
+    }
+  ]
+}
