86aa2ec79ff1af13dccfd7d7ce2fb21afa21e5fa
abishop0894 <abishop0894@gmail.com>
2025-02-23 13:37:33 -0500

    new

--- app/modules/components/auth/components/SignInSection.tsx
+++ app/modules/components/auth/components/SignInSection.tsx
@@ -3,11 +3,11 @@ import Image from "next/image"
 import { SignInButton, SignedOut } from "@clerk/nextjs"
 
 
-const SignInSection = () => {
+const SignInSection = ({polls} : {polls: boolean}) => {
     return (
         <SignedOut>
-        <div className="flex flex-col items-center justify-center min-h-[calc(100vh-24vh)] -z-10">
-            <div className="absolute inset-0 w-full -z-10 mt-[14vh]">
+        <div className="flex flex-col items-center justify-center min-h-[calc(124vh-24vh)] -z-10">
+            <div className="absolute inset-0 w-full -z-10">
               <Image 
                 src="https://local229.s3.us-east-1.amazonaws.com/polls/hblr-train-approaching-the-essex-street-station-in-jersey-city-shutterstock-scaled.jpg"
                 alt="TWU Background"
@@ -17,7 +17,7 @@ const SignInSection = () => {
               <div className="absolute inset-0 bg-[#0a0086] opacity-60" />
             </div>
             <h1 className="text-2xl font-bold text-center text-white z-40">
-              Please <span className="text-[#f5cc00]"><SignInButton>sign in</SignInButton></span> to view and participate in discussions
+              Please <span className="text-[#f5cc00]"><SignInButton>sign in</SignInButton></span> to {polls ? "view polls and statistics" : "view surveys and participate in discussions"} 
             </h1>
           </div>
         </SignedOut>
