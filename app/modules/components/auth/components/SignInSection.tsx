e7f0fe1ee1ac10cdfa36f41669f5db5168f20a3f
abishop0894 <abishop0894@gmail.com>
2025-02-23 13:29:28 -0500

    new

---
+++ app/modules/components/auth/components/SignInSection.tsx
@@ -0,0 +1,27 @@
+"use client"
+import Image from "next/image"
+import { SignInButton, SignedOut } from "@clerk/nextjs"
+
+
+const SignInSection = () => {
+    return (
+        <SignedOut>
+        <div className="flex flex-col items-center justify-center min-h-[calc(100vh-24vh)] -z-10">
+            <div className="absolute inset-0 w-full -z-10">
+              <Image 
+                src="https://local229.s3.us-east-1.amazonaws.com/polls/hblr-train-approaching-the-essex-street-station-in-jersey-city-shutterstock-scaled.jpg"
+                alt="TWU Background"
+                fill
+                className="object-cover -z-10"
+              />
+              <div className="absolute inset-0 bg-[#0a0086] opacity-60" />
+            </div>
+            <h1 className="text-2xl font-bold text-center text-white z-40">
+              Please <span className="text-[#f5cc00]"><SignInButton>sign in</SignInButton></span> to view and participate in discussions
+            </h1>
+          </div>
+        </SignedOut>
+    )
+}
+
+export default SignInSection
