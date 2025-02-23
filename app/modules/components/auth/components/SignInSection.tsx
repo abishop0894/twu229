49be7a2697d8d5f8c00a4924db986b4b5682696c
abishop0894 <abishop0894@gmail.com>
2025-02-23 13:41:50 -0500

    new

--- app/modules/components/auth/components/SignInSection.tsx
+++ app/modules/components/auth/components/SignInSection.tsx
@@ -6,7 +6,7 @@ import { SignInButton, SignedOut } from "@clerk/nextjs"
 const SignInSection = ({polls} : {polls: boolean}) => {
     return (
         <SignedOut>
-        <div className="flex flex-col items-center justify-center min-h-[calc(124vh-24vh)] -z-10">
+        <div className="flex flex-col items-center justify-center min-h-[calc(124vh+40vh)] -z-10">
             <div className="absolute inset-0 w-full -z-10">
               <Image 
                 src="https://local229.s3.us-east-1.amazonaws.com/polls/hblr-train-approaching-the-essex-street-station-in-jersey-city-shutterstock-scaled.jpg"
