430d941d636bd67897b1a1236d53ec36f1d6445b
abishop0894 <abishop0894@gmail.com>
2025-02-23 13:29:28 -0500

    new

--- app/members/survey/page.tsx
+++ app/members/survey/page.tsx
@@ -4,10 +4,10 @@
 import GetInvolved from "@/app/modules/components/polls/GetInvolved";
 import  SurveyComponent  from "@/app/modules/components/polls/SurveyComponent";
 import PageLayout from "@/app/modules/layout/layout-comp";
-import { SignedIn, SignedOut, SignInButton } from "@clerk/nextjs";
-import Image from "next/image";
+import { SignedIn } from "@clerk/nextjs";
 import SurveyResults from "@/app/modules/components/survey/SurveyResults";
-import PollFAQ from "@/app/modules/components/polls/PollFAQ";
+import PollFAQ from "@/app/modules/components/polls/PollFAQ"; 
+import SignInSection from "@/app/modules/components/auth/components/SignInSection";
 export default function SurveyPage() {
   return (
   
@@ -20,23 +20,7 @@ export default function SurveyPage() {
              <PollFAQ />
           <GetInvolved />   
         </SignedIn>
-        <SignedOut>
-    
-          <div className="flex flex-col  items-center justify-center h-[120vh]">
-            <div className="absolute inset-0 w-full flex items-center justify-center pt-[12vh] h-[120vh]">
-              <Image 
-                src="https://local229.s3.us-east-1.amazonaws.com/polls/hblr-train-approaching-the-essex-street-station-in-jersey-city-shutterstock-scaled.jpg"
-                alt="HBLR Train Background"
-                fill
-                className="object-cover h-[120vh] mt-[14vh] -z-10"
-              />
-              <div className="absolute inset-0 bg-[#0a0086] opacity-60" />
-            </div>
-
-            <h1 className="text-2xl font-bold text-center text-white z-40">Please <span className="text-[#f5cc00]"><SignInButton>sign in</SignInButton></span> to view this page</h1>
-          </div>
-      
-        </SignedOut>
+        <SignInSection />
       </PageLayout>
    
 
