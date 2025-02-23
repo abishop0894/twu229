24732f3f7e79a5b2895ae632161f741b5fd5d6a6
abishop0894 <abishop0894@gmail.com>
2025-02-22 21:51:46 -0500

    new

--- app/members/survey/page.tsx
+++ app/members/survey/page.tsx
@@ -22,13 +22,13 @@ export default function SurveyPage() {
         </SignedIn>
         <SignedOut>
     
-          <div className="flex flex-col  items-center justify-center h-[120vh]">
+          <div className="flex flex-col  items-center justify-center h-[120vh] pt-[12vh]">
             <div className="absolute inset-0 w-full h-[120vh]">
               <Image 
                 src="https://local229.s3.us-east-1.amazonaws.com/polls/hblr-train-approaching-the-essex-street-station-in-jersey-city-shutterstock-scaled.jpg"
                 alt="HBLR Train Background"
                 fill
-                className="object-cover h-full -z-10"
+                className="object-cover h-[120vh] -z-10"
               />
               <div className="absolute inset-0 bg-[#0a0086] opacity-60" />
             </div>
